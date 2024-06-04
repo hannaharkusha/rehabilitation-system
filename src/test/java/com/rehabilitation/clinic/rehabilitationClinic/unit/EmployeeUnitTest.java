53969210a7851a4d6b352e6ac28a5156157c5114
weronika <weronika.gawel@onet.eu>
2024-06-04 13:27:06 +0200

    unit tests modified

--- src/test/java/com/rehabilitation/clinic/rehabilitationClinic/unit/EmployeeUnitTest.java
+++ src/test/java/com/rehabilitation/clinic/rehabilitationClinic/unit/EmployeeUnitTest.java
@@ -14,8 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,7 +42,7 @@ public class EmployeeUnitTest {
     }
 
     @Test
-    public void testGetEmployeeById() {
+    public void testGetEmployeeById_ValidId() {
         String plainPassword = "pass";
         String hashedPassword = passwordEncoding.hashPassword(plainPassword);
         Employee employee = new Employee("Jan", "Kowalski", hashedPassword, "Rehabilitant", "email");
@@ -56,7 +55,24 @@ public class EmployeeUnitTest {
     }
 
     @Test
-    public void testAddEmployee() {
+    public void testGetEmployeeById_InvalidId() {
+        int invalidEmployeeId = -1;
+        assertThrows(IllegalArgumentException.class, () -> employeeService.getEmployeeById(invalidEmployeeId));
+    }
+
+    @Test
+    public void testGetEmployeeByEmail() {
+        String email = "jan.k@email.com";
+        Employee employee = new Employee("Jan", "Kowalski", "pass", "RE", email);
+
+        when(employeeRepository.findByEmail(email)).thenReturn(Optional.of(employee));
+        Optional<Employee> result = employeeService.getEmployeeByEmail(email);
+        assertTrue(result.isPresent());
+        assertEquals(employee.getName(), result.get().getName());
+    }
+
+    @Test
+    public void testAddEmployee_ValidData() {
         String plainPassword = "password1";
         String hashedPassword = passwordEncoding.hashPassword(plainPassword);
         Employee employee = new Employee("Jan", "Kowalski", hashedPassword, "DR", "jan.kowalski@example.com");
@@ -67,11 +83,41 @@ public class EmployeeUnitTest {
         verify(employeeRepository, times(1)).save(any(Employee.class));
     }
 
+    @Test
+    public void testAddEmployee_InvalidData() {
+        Employee invalidClient = new Employee(null, null, null, null, null);
+
+        assertThrows(IllegalArgumentException.class, () -> employeeService.addEmployee(
+                invalidClient.getName(),
+                invalidClient.getSurname(),
+                invalidClient.getPassword(),
+                invalidClient.getPosition(),
+                invalidClient.getEmail()
+        ));
+    }
 
     @Test
     public void testDeleteEmployeeById() {
         employeeService.deleteEmployeeById(1);
         verify(employeeRepository, times(1)).deleteById(1);
     }
-}
 
+    @Test
+    public void testEditEmployee_ValidId() {
+        int employeeId = 1;
+        String newName = "Jan";
+        String newSurname = "Kowalski";
+        String newPosition = "DR";
+        String newEmail = "emil@example.com";
+
+        Employee existingEmployee = new Employee("Emil", "Nowak", "pass", "RE", "12345678912");
+        when(employeeRepository.findById(employeeId)).thenReturn(Optional.of(existingEmployee));
+
+        employeeService.editEmployee(employeeId, newName, newSurname, newPosition, newEmail);
+
+        assertEquals(newName, existingEmployee.getName());
+        assertEquals(newSurname, existingEmployee.getSurname());
+        assertEquals(newPosition, existingEmployee.getPosition());
+        assertEquals(newEmail, existingEmployee.getEmail());
+    }
+}
--- src/test/java/com/rehabilitation/clinic/rehabilitationClinic/unit/ServiceUnitTest.java
+++ src/test/java/com/rehabilitation/clinic/rehabilitationClinic/unit/ServiceUnitTest.java
@@ -13,8 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,7 +37,7 @@ public class ServiceUnitTest {
     }
 
     @Test
-    public void testGetServiceById() {
+    public void testGetServiceById_ValidId() {
         Service service = new Service("name", 100);
         service.setServiceId(1);
 
@@ -46,6 +45,12 @@ public class ServiceUnitTest {
         Optional<Service> result = serviceService.getServiceById(1);
         assertTrue(result.isPresent());
         assertEquals(service.getName(), result.get().getName());
+    }
+
+    @Test
+    public void testGetServiceById_InvalidId() {
+        int invalidServiceId = -1;
+        assertThrows(IllegalArgumentException.class, () -> serviceService.getServiceById(invalidServiceId));
     }
 
     @Test
@@ -62,5 +67,52 @@ public class ServiceUnitTest {
     public void testDeleteServiceById() {
         serviceService.deleteServiceById(1);
         verify(serviceRepository, times(1)).deleteById(1);
+    }
+
+    @Test
+    public void testEditService_ValidId() {
+        int serviceId = 1;
+        String newName = "Nowy";
+        float newPrice = 95;
+
+        Service existingService = new Service("Stary", 0);
+        when(serviceRepository.findById(serviceId)).thenReturn(Optional.of(existingService));
+
+        serviceService.editService(serviceId, newName, newPrice);
+
+        assertEquals(newName, existingService.getName());
+        assertEquals(newPrice, existingService.getPrice());
+    }
+
+    @Test
+    public void testEditService_InvalidId() {
+        int invalidServiceId = -1;
+        assertThrows(IllegalArgumentException.class, () -> serviceService.editService(invalidServiceId, "Name", 95));
+    }
+
+    @Test
+    public void testEditServiceName_ValidId() {
+        int serviceId = 1;
+        String newName = "Nowe";
+
+        Service existingService = new Service("Stare", 35);
+        when(serviceRepository.findById(serviceId)).thenReturn(Optional.of(existingService));
+
+        serviceService.editServiceName(serviceId, newName);
+
+        assertEquals(newName, existingService.getName());
+    }
+
+    @Test
+    public void testEditServicePrice_ValidId() {
+        int serviceId = 1;
+        float newPrice = 100;
+
+        Service existingService = new Service("Stare", 35);
+        when(serviceRepository.findById(serviceId)).thenReturn(Optional.of(existingService));
+
+        serviceService.editServicePrice(serviceId, newPrice);
+
+        assertEquals(newPrice, existingService.getPrice());
     }
 }
